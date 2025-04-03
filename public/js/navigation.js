fe1cc83e298250145b9578e2d602b7992939652a
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2025-04-03 00:22:54 -0400

    feat:nicer icons, remove smooth scrolling

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -24,7 +24,6 @@ document.addEventListener('DOMContentLoaded', function() {
         scrollToTopButton.addEventListener('click', function() {
             window.scrollTo({
                 top: 0,
-                behavior: 'smooth' // Optional: Add smooth scrolling
             });
         });
     }
